17b9bc8d7bc169765499031c5bda401d8704fd3a
Suryansh Thakur <suryathakur5002@gmail.com>
2026-03-25 06:43:50 +0530

    Updated API base URL to Render deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Pointing directly to our express server
+  baseURL: 'https://vibe-social-frb9.onrender.com/api', // Pointing directly to our express server
   timeout: 10000
 });
 
